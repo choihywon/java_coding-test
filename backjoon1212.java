97edb5f6b52a8956bef066ee19dd729fcc4c796d
choihywon <144045451+choihywon@users.noreply.github.com>
2023-10-01 23:25:14 +0900

    Add files via upload

---
+++ backjoon1212.java
@@ -0,0 +1,16 @@
+package backjoon1212;
+import java.util.Scanner;
+
+public class backjoon1212 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int oct =  in.nextInt();
+		
+		int bi = 0;
+		
+		
+	}
+
+}
