8398e6a4ff84800649f0340146c62d607a4483bc
choihyewon <dongsim00@gmail.com>
2024-08-15 19:42:52 +0900

    20240814

--- hash stack queue deck heap/zerp/src/Main.java
+++ hash stack queue deck heap/zerp/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 import java.io.*;
 
 public class Main {
@@ -11,16 +12,12 @@ public class Main {
         int K = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < K; i++) {
-            int number = Integer.parseInt(br.readLine());	// 정수 입력
+            int number = Integer.parseInt(br.readLine());
 
-            if(number == 0) {	// 0이라면 스택에 저장된 top 원소를 지운다.
+            if(number == 0) {
                 stack.pop();
             }
             else {
-                /*
-                 * push() 대신 add()로 대체해도 됨 (똑같이 상단에 원소를 추가하는 메소드다.)
-                 * ex) stack.add(number);
-                 */
                 stack.push(number);
             }
         }
