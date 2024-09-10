85f9966ad555c2656a2280534a927397165e0f78
choihyewon <dongsim00@gmail.com>
2024-09-10 21:58:12 +0900

    20240910

---
+++ hash stack queue deck heap/ohkensu/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Stack<Integer> stack = new Stack<Integer>();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n; i++){
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            arr[stack.pop()] = -1;
+        }
+        for(int val : arr) {
+            bw.write(val + " ");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
